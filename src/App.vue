760e88e64c595916a5db36201694ef0d71393964
Alexander Leon <alexanderleon@Alexanders-MacBook-Pro.local>
2023-10-12 22:07:16 -0400

    w

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <button @click="download">Download</button>
+    <div ref="containerRef" class="container" id="pdf"></div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import { useCreateIframeAndLoadViewer } from './Viewer';
+
+export default {
+  name: 'App',
+  setup() {
+    const containerRef = ref(null);
+
+    let download;
+
+    onMounted(() => {
+      const { download: downloadFunc } = useCreateIframeAndLoadViewer({
+        container: containerRef,
+        uuid: 'some-user',
+        licenseKey: 'sandbox',
+        locale: 'en',
+        tools: {
+          editing: ['extract', 'remove', 'move'],
+          thumbnails: ['zoom', 'expand'],
+          general: [
+            'thumbnails',
+            'download',
+            'search',
+            'panel-toggle',
+            'zoom',
+          ],
+        },
+        files: [
+          {
+            url: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
+            name: 'my-file1.pdf',
+          },
+          {
+            url: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
+            name: 'my-file2.pdf',
+          },
+        ],
+      });
+
+      download = downloadFunc;
+    });
+
+    return {
+      containerRef,
+      download,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  /* your styles here */
+}
+</style>
